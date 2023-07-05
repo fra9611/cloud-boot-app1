dc0c9907eaf11f3616f88ddf31824d50b3e55c3c
Francesco Ruscigno <francesco@macbook-pro-di-francesco.local>
2023-07-05 12:28:41 +0200

    init repository

---
+++ Home.py
@@ -0,0 +1,11 @@
+import streamlit as st
+import pandas as pd
+
+st.title("Hello everyone")
+
+st.header("This is a streamlit header")
+
+df = pd.read_csv("https://storage.googleapis.com/cloud-boot-app-bucket/1553768847-housing.csv")
+
+st.table(df) #table o dataframe
+
