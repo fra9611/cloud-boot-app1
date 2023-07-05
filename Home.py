9454462534617d98e4ee5dfd0bafbc7588d8a426
Francesco Ruscigno <francesco@macbook-pro-di-francesco.local>
2023-07-05 12:28:41 +0200

    init repository

--- Home.py
+++ Home.py
@@ -1,19 +1,6 @@
 import streamlit as st
-<<<<<<< HEAD
-import pandas as pd
-
-st.title("Hello everyone")
-
-st.header("This is a streamlit header")
-
-df = pd.read_csv("https://storage.googleapis.com/cloud-boot-app-bucket/1553768847-housing.csv")
-
-st.table(df) #table o dataframe
-
-=======
 
 st.title("Hello ereryone")
 
 st.header("This is a streamlit header")
 
->>>>>>> 318bd56 (init repository)
