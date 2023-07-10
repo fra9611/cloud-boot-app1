91ed5fcf4567bc18b2d514c59b99e6d47350c4a0
francesco <fra9611@gmail.com>
2023-07-10 17:45:26 +0200

    Few changes

--- Home.py
+++ Home.py
@@ -10,13 +10,14 @@ df = pd.read_csv("https://storage.googleapis.com/cloud-boot-app-bucket/155376884
 st.dataframe(df) #table o dataframe
 
 st.subheader()
-selected_option=st.selectbox(
-    "Seleziona opzione:"
+selected_option = st.selectbox(
+    "Seleziona opzione:",
     ["Opzione 1", "Opzione 2"]
 )
 st.write(selected_option)
-selected_options=st.multiselect(
-    "Seleziona opzione:"
+
+selected_options = st.multiselect(
+    "Seleziona opzione:",
     ["Opzione 1", "Opzione 2"]
 )
 st.write(selected_options)
