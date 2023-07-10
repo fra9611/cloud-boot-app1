0c37b25f6d95c01dc179772085e8f41397c3a6c5
francesco <fra9611@gmail.com>
2023-07-10 17:37:34 +0200

    Few changes

--- Home.py
+++ Home.py
@@ -8,3 +8,15 @@ st.header("This is a streamlit header")
 df = pd.read_csv("https://storage.googleapis.com/cloud-boot-app-bucket/1553768847-housing.csv")
 
 st.table(df) #table o dataframe
+
+st.subheader()
+selected_option=st.selectbox(
+    "Seleziona opzione:"
+    ["Opzione 1", "Opzione 2"]
+)
+st.write(selected_option)
+selected_options=st.multiselect(
+    "Seleziona opzione:"
+    ["Opzione 1", "Opzione 2"]
+)
+st.write(selected_options)
