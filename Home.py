a73897b24e728e16f2acd505feb3184de7574e45
francesco <fra9611@gmail.com>
2023-07-10 17:35:22 +0200

    Few changes

--- Home.py
+++ Home.py
@@ -1,6 +1,10 @@
 import streamlit as st
+import pandas as pd
 
 st.title("Hello ereryone")
 
 st.header("This is a streamlit header")
 
+df = pd.read_csv("https://storage.googleapis.com/cloud-boot-app-bucket/1553768847-housing.csv")
+
+st.table(df) #table o dataframe
